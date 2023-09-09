70a56cfa8d1e5244d521994ba3db060eb6f716f7
Corey Fulop <corey.fulop@gmail.com>
2023-09-09 13:54:19 +0100

    Add form validation styles and js logic

--- js/javascript.js
+++ js/javascript.js
@@ -1,2 +1,23 @@
 "use strict";
 
+let passwordField = document.getElementById("password-field");
+let confirmPasswordField = document.getElementById("confirm-password-field");
+let alertHolder = document.getElementById("alert-holder");
+let submitButton = document.querySelector("button");
+
+passwordField.addEventListener("keyup", checkPassword);
+confirmPasswordField.addEventListener("keyup", checkPassword);
+
+
+function checkPassword() {
+    let currentPasswordFieldValue = passwordField.value;
+    let currentConfirmPasswordValue = confirmPasswordField.value;
+    if (currentPasswordFieldValue != currentConfirmPasswordValue) {
+        alertHolder.textContent = "Passwords don't match"
+        submitButton.disabled = true;
+    } else if (currentPasswordFieldValue = currentConfirmPasswordValue) {
+        alertHolder.textContent = "";
+        submitButton.disabled = false;
+    }
+}
+
